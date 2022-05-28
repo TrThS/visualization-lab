e995826d609952658a9735425a693ea705e05e83
TrThS <suong2462@gmail.com>
2022-05-28 19:47:18 +0700

    update modify_data file

--- data/modify_data.py
+++ data/modify_data.py
@@ -10,6 +10,8 @@ def country_to_continent(country_name):
         country_alpha2 = pc.country_name_to_country_alpha2(country_name)
         country_continent_code = pc.country_alpha2_to_continent_code(country_alpha2)
         country_continent_name = pc.convert_continent_code_to_continent_name(country_continent_code)
+        if country_continent_name == 'South America' or country_continent_name == 'North America':
+            country_continent_name = 'America'
     except Exception:
         pass
     return country_continent_name
@@ -19,3 +21,4 @@ continents = [country_to_continent(country)for country in df['Country']]
 df['Continent'] = continents
 
 df.to_csv('data/2022-04-20(modify).csv', index=False)
+
